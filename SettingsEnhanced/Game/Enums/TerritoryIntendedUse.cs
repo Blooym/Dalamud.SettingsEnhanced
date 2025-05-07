b10da9388e0cf4431f1c08658f646ca392f5b4ac
Blooym <19539165+Blooym@users.noreply.github.com>
2025-05-07 10:55:39 +0100

    feat: map even more territory uses to enum

--- SettingsEnhanced/Game/Enums/TerritoryIntendedUse.cs
+++ SettingsEnhanced/Game/Enums/TerritoryIntendedUse.cs
@@ -6,6 +6,7 @@ namespace SettingsEnhanced.Game.Enums
         OpenWorld = 1,
         Inn = 2,
         Dungeon = 3,
+        VariantDungeon = 4,
         JailArea = 5,
         OpeningArea = 6,
         BeforeTrialDung = 7,
@@ -18,6 +19,7 @@ namespace SettingsEnhanced.Game.Enums
         MainStoryQuestPrivateArea = 15,
         Raids = 16,
         RaidFights = 17,
+        Frontline = 18,
         ChocoboSquare = 19,
         ChocoboTutorial = 21,
         Wedding = 22,
@@ -28,18 +30,33 @@ namespace SettingsEnhanced.Game.Enums
         MainStoryQuestEventArea = 29,
         FreeCompanyGarrison = 30,
         PalaceOfTheDead = 31,
+        Unknown1 = 32,
         TreasureMapInstance = 33,
+        Unknown2 = 34,
+        Unknown3 = 35, // Triple triad area?
         EventTrial = 36,
         TheFeastArea = 37,
         DiademV2 = 38,
+        RivalWings = 39,
         PrivateEventArea = 40,
         Eureka = 41,
         TheFeastCrystalTower = 42,
+        Unknown4 = 43,
         LeapOfFaith = 44,
         MaskedCarnival = 45,
         OceanFishing = 46,
         DiademV3 = 47,
         Bozja = 48,
+        IslandSanctuary = 49,
+        Unknown5 = 50, // Triple triad area?
+        Unknown6 = 51, // Triple triad area?
+        Unknown7 = 52, // Delubrum Reginae?
+        Unknown8 = 53,  // Delubrum Reginae?
+        Unknown9 = 54,
+        Unknown10 = 56, // Omicrons?
+        CriterionDungeon = 57,
+        CriterionDungeonSavage = 58,
+        Blunderville = 59,
         CosmicExploration = 60,
     }
 }

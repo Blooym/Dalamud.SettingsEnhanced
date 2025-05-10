8a1283ae3f520b0e9b20a7e6901f0033c3b5a07e
Blooym <19539165+Blooym@users.noreply.github.com>
2025-05-10 11:52:59 +0100

    fix: allow multiple ui elements with the same name

--- SettingsEnhanced/Game/Settings/Attributes/UiSettingPropDisplayAttribute.cs
+++ SettingsEnhanced/Game/Settings/Attributes/UiSettingPropDisplayAttribute.cs
@@ -5,7 +5,7 @@ namespace SettingsEnhanced.Game.Settings.Attributes
     [AttributeUsage(AttributeTargets.Property)]
     internal sealed class UiSettingPropDisplayAttribute(uint nameAddonRow, uint groupAddonRow, uint headerAddonRow, bool interfaceIndented = false) : Attribute
     {
-        public string UiName { get; } = nameAddonRow is 0 ? $"##{Guid.NewGuid()}" : Plugin.AddonSheet.GetRow(nameAddonRow).Text.ExtractText();
+        public string UiName { get; } = nameAddonRow is 0 ? $"##{Guid.NewGuid()}" : $"{Plugin.AddonSheet.GetRow(nameAddonRow).Text.ExtractText()}##{Guid.NewGuid()}";
         public string UiGroup { get; } = groupAddonRow is 0 ? string.Empty : Plugin.AddonSheet.GetRow(groupAddonRow).Text.ExtractText();
         public string UiHeader { get; } = headerAddonRow is 0 ? string.Empty : Plugin.AddonSheet.GetRow(headerAddonRow).Text.ExtractText();
         public bool UiIndented { get; } = interfaceIndented;
